ba44694f6ffbc6be86b84d95ec8f5834b4187e31
ckleinschmidt <ckleinschmidt@unicef.org>
2021-02-25 15:32:21 -0500

    More concise

--- src/Menu/MenuItemExtensions.cs
+++ src/Menu/MenuItemExtensions.cs
@@ -6,34 +6,13 @@ namespace WinMpcTrayIcon.Menu
 {
     public static class MenuItemExtensions
     {
-        public static ToolStripMenuItem ToToolStripMenuItem(this MpcMenuItem command)
-        {
-            var i = new ToolStripMenuItem
-            {
-                Image = command.Image != null ? Image.FromFile(command.Image) : null,
-                Text = command.Text
-            };
-
-            return i;
-        }
-
-        public static ToolStripMenuItem ToToolStripMenuItem(this SwitchMenuItem command)
-        {
-            var i = new ToolStripMenuItem
-            {
-                Image = command.Image != null ? Image.FromFile(command.Image) : null,
-                Text = command.Text
-            };
-
-            return i;
-        }
-
         public static ToolStripMenuItem ToToolStripMenuItem(this MenuItem command)
         {
-            var i = new ToolStripMenuItem
-            {
-                Text = command.Text
-            };
+            var i = new ToolStripMenuItem();
+            i.Text = command.Text;
+
+            if (command.GetType() == typeof(MpcMenuItem))
+                i.Image = ((MpcMenuItem)command).Image != null ? Image.FromFile(((MpcMenuItem)command).Image) : null;
 
             return i;
         }
